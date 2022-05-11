d7a3b9f66c98b0220399dadf79835085efcc83bf
SyrdiiDima <dimasyrdij@icloud.com>
2022-05-11 23:32:19 +0300

    task 2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from "lodash.throttle";
 
+
+const videoPlayer = document.querySelector("#vimeo-player");
+
+const player = new Vimeo.Player(videoPlayer);
+ 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+function onPlay({ seconds }) {
+    localStorage.setItem("videoplayer-current-time",seconds)
+
+}
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
